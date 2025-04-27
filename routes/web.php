e58c8a8e07a3dcd3377270f3176dce35dcb416f9
Arminium92 <hadesium02@gmail.com>
2025-04-27 13:38:02 +0330

    Created Routes, need to refactor and complete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,5 +14,24 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+
+
+// Auth Routes
+Route::resource('posts', Post::class)->middleware('auth')->except('index', 'show');
+
+Route::resource('categories', Category::class)->middleware('auth')->except('index', 'show');
+
+Route::resource('comments', Comment::class)->middleware('auth')->except('index', 'show');
+
+
+// Public Routes
+Route::resource('posts', Post::class)->only('index', 'show');
+
+Route::resource('categories', Category::class)->only('index', 'show');
+
+Route::resource('comments', Comment::class)->only('index', 'show');
+
+
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
