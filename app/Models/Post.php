9497c1ab192de41463ea1cac8295572fdfd27922
Arminium92 <hadesium02@gmail.com>
2025-05-04 13:16:50 +0330

    Add soft delete for posts and comments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+  use SoftDeletes;
   public function category()
   {
     return $this->belongsTo(Category::class);
